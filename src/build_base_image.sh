6dba88c33c310def515eb4e808bf37673de68ccc
Eugenio Paolantonio (g7) <me@medesimo.eu>
2024-05-12 13:15:41 +0200

    [docker-images] Add Droidian base images generated from snapshots
    Currently only the "next" flavour is used.
    
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

---
+++ src/build_base_image.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+set -e
+
+info() {
+	echo "I: $@"
+}
+
+warning() {
+	echo "W: $@" >&2
+}
+
+error() {
+	echo "E: $@" >&2
+	exit 1
+}
+
+HOST_ARCH="$(uname -m)"
+ARCH="${1}"
+FLAVOUR="${2}"
+[ -n "${FLAVOUR}" ] || error "USAGE: ${0} <arch> <flavour>"
+
+info "Building Droidian base image for ${ARCH} (flavour: ${FLAVOUR})"
+
+tmpdir="$(mktemp -d)"
+onexit() {
+	rm -rf ${tmpdir}
+}
+trap onexit EXIT
+
+git clone https://salsa.debian.org/installer-team/debootstrap.git ${tmpdir}/debootstrap --depth 1
+cp  ${tmpdir}/debootstrap/scripts/sid ${tmpdir}/debootstrap/scripts/rolling
+
+mkdir -p "base-${ARCH}-${FLAVOUR}"
+DEBOOTSTRAP_DIR=${tmpdir}/debootstrap \
+${tmpdir}/debootstrap/debootstrap \
+	--foreign \
+	--arch="${ARCH}" \
+	--components=main \
+	--variant=minbase \
+	--include=droidian-archive-keyring,ca-certificates,droidian-release-snapshot-control \
+	--keyring=keyrings/all/droidian.gpg \
+	rolling \
+	base-${ARCH}-${FLAVOUR} \
+	http://releases.droidian.org/snapshots/${FLAVOUR}
+
+case "${ARCH}" in
+	"arm64")
+		[ "${HOST_ARCH}" == "aarch64" ] || QEMU_STATIC_EXECUTABLE="/usr/bin/qemu-aarch64-static"
+		;;
+	"armhf")
+		[ "${HOST_ARCH}" == "arm" ] || QEMU_STATIC_EXECUTABLE="/usr/bin/qemu-arm-static"
+		;;
+	"amd64")
+		[ "${HOST_ARCH}" == "x86_64" ] || QEMU_STATIC_EXECUTABLE="/usr/bin/qemu-x86_64-static"
+		;;
+	"*")
+		error "Arch ${ARCH} not supported"
+		;;
+esac
+
+[ -z "${QEMU_STATIC_EXECUTABLE}" ] || cp "${QEMU_STATIC_EXECUTABLE}" base-${ARCH}-${FLAVOUR}/${QEMU_STATIC_EXECUTABLE}
+
+chroot base-${ARCH}-${FLAVOUR} \
+/debootstrap/debootstrap \
+	--second-stage
+
+# Cleanup
+chroot base-${ARCH}-${FLAVOUR} \
+apt-get clean
+
+[ -z "${QEMU_STATIC_EXECUTABLE}" ] || rm -f base-${ARCH}-${FLAVOUR}/${QEMU_STATIC_EXECUTABLE}
+
+tar caf base-${ARCH}-${FLAVOUR}.tar -C base-${ARCH}-${FLAVOUR} .
+rm -rf base-${ARCH}-${FLAVOUR}
--- src/build_from_template.py
+++ src/build_from_template.py
@@ -7,7 +7,10 @@ import os
 
 from collections import namedtuple
 
-YAML_FILE = "./.github/workflows/docker-images.yml"
+YAML_FILES = [
+	"./.github/workflows/base-images.yml",
+	"./.github/workflows/docker-images.yml",
+]
 DOCKERFILE_PATH = "./Dockerfile.%s.in"
 DOCKERFILE_TARGET = "./Dockerfile.%(target_name)s"
 
@@ -15,64 +18,65 @@ DOCKERFILE_TARGET = "./Dockerfile.%(target_name)s"
 if __name__ != "__main__":
 	raise Exception("You shouldn't import this script")
 
-if not os.path.exists(YAML_FILE):
-	raise Exception("Unable to find %s" % YAML_FILE)
+for yaml_file in YAML_FILES:
+	if not os.path.exists(yaml_file):
+		raise Exception("Unable to find %s" % YAML_FILE)
 
-with open(YAML_FILE, "r") as f:
-	content = yaml.load(f, Loader=yaml.FullLoader)
+	with open(yaml_file, "r") as f:
+		content = yaml.load(f, Loader=yaml.FullLoader)
 
-	templates_mapping = {}
+		templates_mapping = {}
 
-	matrix_block = content.get("jobs", {"build" : {}})["build"].get("strategy", {"matrix": []})["matrix"]
-	exclude = matrix_block.pop("exclude")
-	if not matrix_block:
-		raise Exception("No matrix block found")
+		matrix_block = content.get("jobs", {"build" : {}})["build"].get("strategy", {"matrix": []})["matrix"]
+		exclude = matrix_block.pop("exclude", [])
+		if not matrix_block:
+			raise Exception("No matrix block found")
 
-	matrix = [{}]
-	for item, content in matrix_block.items():
-		_matrix = []
-		for matrix_item in matrix:
-			for content_item in content:
-				_matrix.append({**matrix_item, **{item: content_item}})
+		matrix = [{}]
+		for item, content in matrix_block.items():
+			_matrix = []
+			for matrix_item in matrix:
+				for content_item in content:
+					_matrix.append({**matrix_item, **{item: content_item}})
 
-		matrix = _matrix
+			matrix = _matrix
 
-	# Parse exclusions
-	excluded = []
-	for job in matrix:
-		for excluded_block in exclude:
-			matched = [job[x] == y for x, y in excluded_block.items()]
-			if not False in matched:
-				excluded.append(job)
+		# Parse exclusions
+		excluded = []
+		for job in matrix:
+			for excluded_block in exclude:
+				matched = [job[x] == y for x, y in excluded_block.items()]
+				if not False in matched:
+					excluded.append(job)
 
-	print("The following jobs are excluded: %s" % excluded)
+		print("The following jobs are excluded: %s" % excluded)
 
-	for job in matrix:
-		if job in excluded:
-			continue
+		for job in matrix:
+			if job in excluded:
+				continue
 
-		target_name = ("%s_%s_%s_%s" % (
-			job["arch"],
-			job["namespace"],
-			job["template"],
-			job["dist"],
-		)).replace("/", "_").replace(":", "_").replace(".", "_").replace("-","_")
+			target_name = ("%s_%s_%s_%s" % (
+				job["arch"],
+				job["namespace"],
+				job["template"],
+				job["dist"],
+			)).replace("/", "_").replace(":", "_").replace(".", "_").replace("-","_")
 
-		templates_mapping.setdefault(job["template"], []).append(
-			{
-				"target_name" : target_name,
-				"arch" : job["arch"],
-				"namespace" : job["namespace"],
-				"template" : job["template"],
-				"tag" : job["dist"],
-			}
-		)
+			templates_mapping.setdefault(job["template"], []).append(
+				{
+					"target_name" : target_name,
+					"arch" : job["arch"],
+					"namespace" : job["namespace"],
+					"template" : job["template"],
+					"tag" : job["dist"],
+				}
+			)
 
-	for template, targets in templates_mapping.items():
-		# Load template
-		with open(DOCKERFILE_PATH % template, "r") as t:
-			template_content = t.read()
+		for template, targets in templates_mapping.items():
+			# Load template
+			with open(DOCKERFILE_PATH % template, "r") as t:
+				template_content = t.read()
 
-			for target in targets:
-				with open(DOCKERFILE_TARGET % target, "w") as a:
-					a.write(template_content % target)
+				for target in targets:
+					with open(DOCKERFILE_TARGET % target, "w") as a:
+						a.write(template_content % target)
